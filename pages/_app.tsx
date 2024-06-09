26f3aca2a54d45984cd885a77741a110c4fd0442
Huy Do <herohthd@gmail.com>
2024-06-09 13:56:10 +0700

    update app layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,42 @@
 import "../styles/app.sass";
-import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import type { AppProps } from "next/app";
+import '@rainbow-me/rainbowkit/styles.css';
+import { UserProvider } from "context/user";
+import { WalletProvider } from "context/wallet";
+import { RainbowKitProvider, DisclaimerComponent } from '@rainbow-me/rainbowkit';
+import { WagmiConfig } from 'wagmi'
+import React from 'react'
+import { wagmiConfig, chains } from "@/config/wagmi";
+import { customTheme } from "@/config/theme";
+
+const Disclaimer: DisclaimerComponent = ({ Text, Link }) => (
+  <Text>
+    We <strong>HIGHLY</strong> reccomend everyone use desktop and the metamask browser extension at the moment
+  </Text>
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <Layout layoutNoOverflow>
-            <Component {...pageProps} />
-        </Layout>
-    )
-    
-    ;
+        <WagmiConfig config={wagmiConfig}>
+            <RainbowKitProvider 
+                chains={chains}
+                theme={customTheme}
+                appInfo={{
+                  appName: 'Ubiquity',
+                  disclaimer: Disclaimer,
+                }}
+                modalSize="compact">
+                  <WalletProvider>
+                    <UserProvider>
+                      <Layout layoutNoOverflow footerHide={false}>
+                        <Component {...pageProps}/>
+                      </Layout>
+                    </UserProvider>
+                  </WalletProvider>
+            </RainbowKitProvider>
+        </WagmiConfig>
+    );
 }
 
 export default MyApp;
