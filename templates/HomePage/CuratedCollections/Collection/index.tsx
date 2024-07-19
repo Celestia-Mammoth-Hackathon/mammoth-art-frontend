b774788f0620d62b840484d961c2d458d2f15003
Huy Do <herohthd@gmail.com>
2024-07-19 22:35:37 +0700

    Card Text Update

--- templates/HomePage/CuratedCollections/Collection/index.tsx
+++ templates/HomePage/CuratedCollections/Collection/index.tsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import Link from "next/link";
 import styles from "./Collection.module.sass";
 import Image from "@/components/Image";
 import { formatUserAddress } from "@/utils/index";
 import { handleIpfsLink } from "@/utils/index";
 import { Stream } from "@cloudflare/stream-react";
+import { artistAddresses } from '@/constants/details';
 
 type CollectionProps = {
     item: any;
 };
 
 const Collection = ({ item }: CollectionProps) => {
+    const [isAuthorHovered, setIsAuthorHovered] = useState(false);
+
     const href = item.showCollection
         ? {
             pathname: '/collection/[slug]',
@@ -19,26 +23,33 @@ const Collection = ({ item }: CollectionProps) => {
             pathname: '/drop/[slug]',
             query: { slug: item.slug },
         };
+    const artistInfor:any = artistAddresses.find((element) => element.artistAddress === item.contractCreator) || {};
 
     return (
-        <Link href={{ pathname: href.pathname, query: href.query}}>
-            <a className={styles.collection}>
-                <div className={styles.images}>
-                    <div className={styles.image}>
-                        <Image
-                            className={styles.photo}
-                            src={handleIpfsLink(item.metadata.image)}
-                            layout="fill"
-                            objectFit="contain"
-                            alt="Curated Collection"
-                            priority={true}
-                        />
+        <>
+            <div className={styles.collection}>
+                <Link href={{ pathname: href.pathname, query: href.query}}>
+                    <div className={styles.images}>
+                        <div className={styles.image}>
+                            <Image
+                                className={styles.photo}
+                                src={handleIpfsLink(item.metadata.image)}
+                                layout="fill"
+                                objectFit="contain"
+                                alt="Curated Collection"
+                                priority={true}
+                            />
+                        </div>
                     </div>
-                </div>
+                </Link>
                 <div className={styles.details}>
                     <div className={styles.box}>
-                        <div className={styles.subtitle}>{item.metadata.name}</div>
-                        <div className={styles.author}>
+                        <Link href={{ pathname: href.pathname, query: href.query}}>
+                            <div className={`${styles.subtitle} ${isAuthorHovered ? styles.subtitleHover : ''}`}>
+                                {item.metadata.name}
+                            </div>
+                        </Link>
+                        <div className={styles.authorWrapper}>
                             {/* <div className={styles.avatar}>
                                 <Image
                                     src={item.avatar}
@@ -47,16 +58,36 @@ const Collection = ({ item }: CollectionProps) => {
                                     alt="Avatar"
                                 />
                             </div> */}
-                            {formatUserAddress(item.contractCreator)}
+                            <span className={styles.byAuthor}>By: </span>
+                            <Link href={{
+                                    pathname: '/profile/[slug]',
+                                    query: { slug: artistInfor?.slug },
+                                }}>
+                                    <span 
+                                        className={styles.author}
+                                        onMouseEnter={() => setIsAuthorHovered(true)}
+                                        onMouseLeave={() => setIsAuthorHovered(false)}
+                                    >{artistInfor?.name || formatUserAddress(item.contractCreator)}</span>
+                                </Link> 
                         </div>
                     </div>
+                    <div className={styles.line}></div>
                     <div className={styles.box}>
-                        <div className={styles.text}>{item.mintedSupply}x</div>
-                        <div className={styles.price}>{item.price} TIA</div>
+                        <div>
+                            <div className={styles.label}>Price</div>
+                            <div className={styles.price}>{item.price} TIA</div>
+                        </div>
+                        <div>
+                            <div className={styles.label}>Mints</div>
+                            <div className={styles.text}>{item.mintedSupply}</div>
+                        </div>
+                        
+                        
                     </div>
                 </div>
-            </a>
-        </Link>
+            </div>
+        </>
+        
 )};
 
 export default Collection;
