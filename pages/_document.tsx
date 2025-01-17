0256adbc6412a8f105a87dcc1f593a5d5a7a055e
HuyDo <herohthd@gmail.com>
2025-01-17 15:26:15 +0100

    update logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,11 +18,11 @@ class MyDocument extends Document {
                 <Head>
                     <meta
                         content="Create Anything"
-                        name="MammothArt- NFT Marketplace. Create Anything.
+                        name="MammothArt- Generative Art NFT Marketplace.
                             Powered by Celestia"
                     />
                     <meta
-                        content="MammothArt- NFT Marketplace. Create Anything.
+                        content="MammothArt- Generative Art NFT Marketplace.
                                 Powered by Celestia"
                         property="og:title"
                     />
@@ -40,11 +40,11 @@ class MyDocument extends Document {
                     />
                     <meta
                         property="og:site_name"
-                        content="MammothArt- NFT Marketplace. Create Anything.
+                        content="MammothArt- Generative Art NFT Marketplace.
                             Powered by Celestia"
                     />
                     <meta
-                        content="MammothArt- NFT Marketplace. Create Anything.
+                        content="MammothArt- Generative Art NFT Marketplace.
                             Powered by Celestia"
                         property="twitter:title"
                     />
