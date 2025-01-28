44a961fbc69690261bd3028802ec34d2191a482e
HuyDo <herohthd@gmail.com>
2025-01-28 13:45:45 +0100

    add details page

---
+++ pages/details/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import DetailsPage from "@/templates/DetailsPage";
+
+const Details: NextPage = () => {
+    return <DetailsPage/>;
+    
+
+};
+
+export default Details;
