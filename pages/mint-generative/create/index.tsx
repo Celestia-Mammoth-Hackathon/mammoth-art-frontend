31335237c5a3f5fffbfb6bc33a1cd4b5a7802261
HuyDo <herohthd@gmail.com>
2025-03-08 12:22:40 +0100

    feat: Add mobile detection for Mint Generative Create page
    - Implement mobile detection using useEffect and window resize event
    - Conditionally render VisitOnDesktopPage for mobile devices
    - Create VisitOnDesktopPage component with responsive styling

--- pages/mint-generative/create/index.tsx
+++ pages/mint-generative/create/index.tsx
@@ -2,10 +2,35 @@ import type { NextPage } from "next";
 import CreatePage from "@/templates/MintGenerativePage/Create/CreatePage";
 import PreviewPage from "@/templates/MintGenerativePage/Create/PreviewPage";
 import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
+import VisitOnDesktopPage from "@/templates/VisitOnDesktopPage";
 
 const Create: NextPage = () => {
     const router = useRouter();
+    const [isMobile, setIsMobile] = useState<boolean>(false);
     const { cid } = router.query;
+
+    useEffect(() => {
+        const checkIsMobile = () => {
+            if (typeof window !== "undefined") {
+                setIsMobile(window.innerWidth <= 1023);
+            }
+        };
+
+        checkIsMobile();
+
+        // Add event listener to check for mobile resizing
+        window.addEventListener('resize', checkIsMobile);
+
+        // Cleanup the event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', checkIsMobile);
+        };
+    }, []);
+
+    if(isMobile) {
+        return <VisitOnDesktopPage />;
+    }
 
     if(cid) {
         return <PreviewPage cid={cid}/>;
---
+++ templates/VisitOnDesktopPage/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./VisitOnDesktopPage.module.sass";
+
+const VisitOnDesktopPage = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <span>Visit us on Desktop for the best experience</span>
+      </div>
+    </div>
+  );
+}
+
+export default VisitOnDesktopPage;
